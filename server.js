00d5b4929739de2ec5f11bf0d9c13feb6d1020ac
Ricky Kusriana Subagja <rickykusriana@gmail.com>
2018-11-14 01:29:14 +0700

    Change server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 8080;
 app.listen(port);
