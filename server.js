640abf63c21cf3f84701a41c9a10ed67191b04fb
Ricky Kusriana Subagja <rickykusriana@gmail.com>
2018-11-14 01:24:43 +0700

    Init to heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+var express = require('express');
+var serveStatic = require('serve-static');
+var path = require('path');
+
+app = express();
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log('server started '+ port);
